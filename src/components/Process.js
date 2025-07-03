6d5588b0d7d7cebcff5cdb1111c1205f212e5edb
Twoje Imię <twojemail@example.com>
2025-07-03 15:46:27 +0200

    Rozwiązanie konfliktów po pull

--- src/components/Process.js
+++ src/components/Process.js
@@ -3,7 +3,22 @@ import React from "react";
 const Proces = () => {
 
     return(
-        <div>Siema</div>
+        <div>
+                <div className=" container ui segment">
+                    <div className="estimate-title-box">PRZEBIEG WSPÓŁPRACY</div>
+                        <div>
+                                <p>Kontakt i darmowa wycena</p>
+                                <p>Specjalizujemy się w profesjonalnym malowaniu – działamy na terenie Wrocławia i okolic.
+            Wypełnij nasz formularz online lub zadzwoń, a skontaktujemy się z Tobą, by umówić dogodny termin spotkania.
+            Zaufaj fachowcom od malowania wnętrz i elewacji</p>
+                        </div>
+                        <div>
+                            <p>Wizyta i finalizacja oferty</p>
+                            <p>Podczas spotkania na terenie Wrocławiai i okolicach Wrocławia oceniamy stan techniczny powierzchni przeznaczonych do malowania, 
+                                omawiamy zakres prac i przygotowujemy precyzyjną wycenę naszych usług malarskich.</p>
+                        </div>
+                </div>
+        </div>
     )
 }
 
