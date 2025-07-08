f1ac2b0f8684a172656ac2e8f6cd26a5ea88df5d
Twoje Imię <twojemail@example.com>
2025-07-08 15:39:59 +0200

    Rozwiązanie konfliktów po pull

--- src/components/NavbarMobile.js
+++ src/components/NavbarMobile.js
@@ -7,9 +7,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import kolorsystem from '../img/kolorsystem.png'
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 function NavbarMobile() {
   const expand = 'lg'; // działa dla < 992px
+  const [show, setShow] = useState(false);
   const navigate = useNavigate();
+  const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
   return (
     <Navbar fixed="top"  bg='white' expand={expand} className="d-block d-lg-none nav-mobile">
       <Container fluid>
@@ -20,14 +24,15 @@ function NavbarMobile() {
         </Navbar.Brand>
 
         
-        <Navbar.Toggle  aria-controls={`offcanvasNavbar-expand-${expand}`} />
+        <Navbar.Toggle  aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={handleShow} />
 
         {/* Offcanvas (po kliknięciu burgera) */}
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-${expand}`}
           aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
           placement="end"
-          
+          show={show}
+          onHide={handleClose}
         >
           <Offcanvas.Header  closeButton>
             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
@@ -36,9 +41,9 @@ function NavbarMobile() {
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3 ">
-              <Nav.Link className='nav-links-mobile'   href="#home" onClick={()=> navigate("/")}>Strona Główna</Nav.Link>
-              <Nav.Link className='nav-links-mobile' onClick={() =>{navigate("/estimate")}} href="#about">Wycena</Nav.Link>
-              <Nav.Link className='nav-links-mobile'  onClick={()=> navigate("/contactus")} href="#contact">Kontakt</Nav.Link>
+              <Nav.Link className='nav-links-mobile'   href="#home" onClick={()=> {handleClose(); navigate("/")}}>Strona Główna</Nav.Link>
+              <Nav.Link className='nav-links-mobile' onClick={() =>{handleClose();navigate("/estimate")}} href="#about">Wycena</Nav.Link>
+              <Nav.Link className='nav-links-mobile'  onClick={()=>{handleClose(); navigate("/contactus")}} href="#contact">Kontakt</Nav.Link>
             </Nav>
             {/* <Form className="d-flex mt-3">
               <Form.Control type="search" placeholder="Search" className="me-2" />
