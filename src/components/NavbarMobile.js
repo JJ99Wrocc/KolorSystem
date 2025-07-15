9d0f003a7c557d0a7a9d470f89b4dd3e47ebabd1
Twoje Imię <twojemail@example.com>
2025-07-15 07:14:56 +0200

    Rozwiązanie konfliktów po pull

--- src/components/NavbarMobile.js
+++ src/components/NavbarMobile.js
@@ -5,28 +5,53 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import kolorsystem from '../img/kolorsystem.png'
+import kolorsystem from '../img/kolorsystem.png';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 function NavbarMobile() {
-  const expand = 'lg'; // działa dla < 992px
+  const expand = 'lg'; 
   const [show, setShow] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
   const navigate = useNavigate();
+
   const handleClose = () => setShow(false);
-const handleShow = () => setShow(true);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log('scrollY:', window.scrollY);
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    handleScroll(); 
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <Navbar fixed="top"  bg='white' expand={expand} className="d-block d-lg-none nav-mobile">
+    <Navbar
+      fixed="top"
+      bg="white"
+      expand={expand}
+      className={`d-block d-lg-none nav-mobile ${isVisible ? 'visible' : ''}`}
+    >
       <Container fluid>
-        {/* Brand z lewej */}
         <Navbar.Brand href="#">
-            <img className='nav-mobile-logo' src={kolorsystem}>
-            </img>
+          <img className="nav-mobile-logo" src={kolorsystem} alt="KolorSystem logo" />
         </Navbar.Brand>
 
-        
-        <Navbar.Toggle  aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={handleShow} />
+        {/* Burger */}
+        <Navbar.Toggle
+          aria-controls={`offcanvasNavbar-expand-${expand}`}
+          onClick={handleShow}
+        />
 
-        {/* Offcanvas (po kliknięciu burgera) */}
+        {/* Offcanvas menu */}
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-${expand}`}
           aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
@@ -34,21 +59,52 @@ const handleShow = () => setShow(true);
           show={show}
           onHide={handleClose}
         >
-          <Offcanvas.Header  closeButton>
+          <Offcanvas.Header closeButton>
             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-              Menu
+              Men
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3 ">
-              <Nav.Link className='nav-links-mobile'   href="#home" onClick={()=> {handleClose(); navigate("/")}}>Strona Główna</Nav.Link>
-              <Nav.Link className='nav-links-mobile' onClick={() =>{handleClose();navigate("/estimate")}} href="#about">Wycena</Nav.Link>
-              <Nav.Link className='nav-links-mobile'  onClick={()=>{handleClose(); navigate("/contactus")}} href="#contact">Kontakt</Nav.Link>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link
+                className="nav-links-mobile"
+                href="#home"
+                onClick={() => {
+                  handleClose();
+                  navigate('/');
+                }}
+              >
+                Strona Główna
+              </Nav.Link>
+              <Nav.Link
+                className="nav-links-mobile"
+                href="#about"
+                onClick={() => {
+                  handleClose();
+                  navigate('/estimate');
+                }}
+              >
+                Wycena
+              </Nav.Link>
+              <Nav.Link
+                className="nav-links-mobile"
+                href="#contact"
+                onClick={() => {
+                  handleClose();
+                  navigate('/contactus');
+                }}
+              >
+                Kontakt
+              </Nav.Link>
             </Nav>
-            {/* <Form className="d-flex mt-3">
+
+            {/* Search form (opcjonalnie odkomentuj jeśli chcesz) */}
+            {/* 
+            <Form className="d-flex mt-3">
               <Form.Control type="search" placeholder="Search" className="me-2" />
               <Button variant="outline-success">Search</Button>
-            </Form> */}
+            </Form> 
+            */}
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
