7573e7ad7d4a714438a6671538bc60768fa81549
Twoje Imię <twojemail@example.com>
2025-07-22 14:24:03 +0200

    Rozwiązanie konfliktów po pull

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React, { useEffect, Suspense, lazy } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Helmet } from "react-helmet";
-
-// Stałe komponenty
+import React, {useEffect} from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import NavbarWeb from "./components/Navbar";
 import NavbarMobile from "./components/NavbarMobile";
+import Headers from "./components/Header";
+import Estimate from "./components/estimate";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./components/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
+import WhyUs from "./components/WhyUs";
+import Picture from "./components/picture";
+// import MySwiper from "./components/MySwiper";
+import Proces from "./components/Process";
+import ContactUs from "./components/ContactUs";
+import NavBarNavConst from "./components/NavbarNavConst";
 
-// Dynamiczne importy (code splitting)
-const Headers = lazy(() => import("./components/Header"));
-const Estimate = lazy(() => import("./components/estimate"));
-const WhyUs = lazy(() => import("./components/WhyUs"));
-const Picture = lazy(() => import("./components/picture"));
-const Proces = lazy(() => import("./components/Process"));
-const ContactUs = lazy(() => import("./components/ContactUs"));
 
-// Bootstrap ładowany lokalnie przez npm
 
 function LayoutWithNavAndFooter({ children }) {
   return (
+    
     <>
+      
       <NavbarWeb />
       <NavbarMobile />
+
       {children}
+    
     </>
   );
 }
 
-function AppRoutes() {
-  return (
+function AppRoutes(){
+  return(
     <Routes>
       <Route
         index
@@ -42,74 +43,37 @@ function AppRoutes() {
           </LayoutWithNavAndFooter>
         }
       />
-      <Route
-        path="/estimate"
-        element={
-          <LayoutWithNavAndFooter>
-            <Estimate />
-          </LayoutWithNavAndFooter>
-        }
-      />
-      <Route
-        path="/contactus"
-        element={
-          <LayoutWithNavAndFooter>
-            <ContactUs />
-          </LayoutWithNavAndFooter>
-        }
-      />
-      <Route
-        path="/headers"
-        element={
-          <LayoutWithNavAndFooter>
-            <Headers />
-          </LayoutWithNavAndFooter>
-        }
-      />
+      <Route path="/estimate" element={
+        <LayoutWithNavAndFooter>
+          <Estimate />
+        </LayoutWithNavAndFooter>
+      } />
+      
+      <Route path="/contactus" element={
+        <LayoutWithNavAndFooter>
+          <ContactUs />
+        </LayoutWithNavAndFooter>
+      } />
+      <Route path="/headers" element={
+        <LayoutWithNavAndFooter>
+          <Headers />
+        </LayoutWithNavAndFooter>
+      } />
+
     </Routes>
-  );
+  )
 }
 
+
 function App() {
-  useEffect(() => {
-    const loadResources = async () => {
-      const AOS = await import("aos");
-      await import("aos/dist/aos.css");
-      AOS.init({ duration: 1000 });
-
-      await import("semantic-ui-css/semantic.min.css");
-    };
-
-    loadResources();
-  }, []);
-
   return (
     <div className="App">
-      {/* Helmet do preloadowania czcionki Google */}
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
-          onLoad="this.onload=null;this.rel='stylesheet'"
-        />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" />`,
-          }}
-        />
-      </Helmet>
-
       <BrowserRouter basename="/KolorSystem">
-        <div className="content">
-          <Suspense fallback={<div>Ładowanie...</div>}>
-            <AppRoutes />
-          </Suspense>
+      <div className="content">
+          <AppRoutes />
         </div>
         <Footer />
-      </BrowserRouter>
+      </BrowserRouter >
     </div>
   );
 }
