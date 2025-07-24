f833e4ad1947ccf8a2475b885ffcb94ce5f7826f
Twoje Imię <twojemail@example.com>
2025-07-24 11:17:00 +0200

    Rozwiązanie konfliktów po pull

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,70 @@
-    import React, { useEffect, useState } from "react";
-    import headerXL from "../img/headerXL.webp"
-
+import React, { useEffect, useState } from "react";
+import headerM from "../img/headerM.webp";
+import headerL  from "../img/headerL.webp"
+import headerXL from "../img/headerXL.webp";
+import headerXXL from '../img/headerXXL.webp'
 
 import NavBarNavConst from "./NavbarNavConst";
 import NavbarWebConst from "./NavbarWebConst";
 import { useNavigate } from "react-router-dom";
 
+const Headers = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY < 99);
+    };
 
-    const Headers = () => {
-            const [isVisible, setIsVisible] = useState(false);
-            const navigate = useNavigate();
-            useEffect(() =>{
-                const handleScroll = () => {
-                    if(window.scrollY < 99 ){
-                        setIsVisible(true)
-                    } else {
-                        setIsVisible(false)
-                    }
+    handleScroll(); // uruchom na start
+    window.addEventListener("scroll", handleScroll);
 
-                    handleScroll();
-                    window.addEventListener('scroll', handleScroll);
-                    return () => window.removeEventListener('scroll', handleScroll)
-                }
-            })
-        return( 
-            <div className="wrapper">
-                <img className="header-img lg-none d-none d-lg-block" src={headerXL} alt="header-img" fetchpriority="high" >
-                </img>
-                <button className="order-header-btn"
-                        onClick={() => navigate('/estimate')}                
-                >UMÓW BEZPŁATNĄ WYCENE</button>
-                <div className="header d-block d-lg-none">
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-            <NavBarNavConst />
+  return (
+    <div className="wrapper hero-wrapper">
+      <div className="header d-block d-lg-none">
+        <NavBarNavConst />
+      </div>
+
+      {/* Nawigacja desktopowa */}
+      <div>
+        <NavbarWebConst />
+      </div>
+      {/* Zdjęcia dla desktop / mobile */}
+      <img
+        className="header-img d-block d-lg-none"
+        src={headerM}
+        alt="header-m"
+        fetchpriority="high"
+      />
+    
+      <img
+        className="header-img d-none d-lg-block d-xl-none"
+        src={headerXL}
+        alt="header-xl"
+        fetchpriority="high"
+      />
+      <img 
+      className=" header-img d-none d-xl-block"
+      src={headerXXL}
+      alt="header-xxl"
+      fetchpriority="high"
+      />
+
+      {/* Przycisk */}
+      <button
+        className="order-header-btn"
+        onClick={() => navigate("/estimate")}
+      >
+        UMÓW BEZPŁATNĄ WYCENĘ
+      </button>
+
+      {/* Nawigacja mobilna */}
     </div>
-                <div    >
-                    <NavbarWebConst />
-                </div>
-            </div>
-        )
-    }
+  );
+};
 
-    export default Headers;
+export default Headers;
