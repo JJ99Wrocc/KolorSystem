eb72921dda020ed9b539531f29a91e9827bac2ff
Twoje Imię <twojemail@example.com>
2025-07-24 16:47:52 +0200

    Rozwiązanie konfliktów po pull

--- src/components/NavbarMobile.js
+++ src/components/NavbarMobile.js
@@ -39,7 +39,7 @@ function NavbarMobile() {
       bg="white"
       expand={expand}
       className={`d-block d-lg-none nav-mobile ${isVisible ? 'visible' : ''}`}
-      style={{zIndex:'99999999999'}}
+      style={{zIndex:'2'}}
     >
       <Container fluid>
         <Navbar.Brand href="#">
