2088bc3c1e17850e21e33cea8f9780caac115b07
Twoje Imię <twojemail@example.com>
2025-08-04 13:51:30 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'swiper/css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration.js/serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration.js/serviceWorkerRegistration';
 
 
 
